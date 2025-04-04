9eadda64050bed748a55e0de6c42951f864aedb3
ms-kaylaa <108439098+ms-kaylaa@users.noreply.github.com>
2025-04-04 18:32:19 -0400

    shit!

--- src/util/awesome_util.py
+++ src/util/awesome_util.py
@@ -11,13 +11,14 @@ def lerp(a, b, f):
 def clamp(val, low, high):
     return max(low, min(val, high))
 
-def play_snd(file):
+def play_snd(file:str):
     """
     plays a sound located in `globals.SND_DIRECTORY`, taking into account the current game volume
 
     returns the pygame.Channel instance in case you wanna do something with it
     """
-
+    if not file.endswith(".ogg"):
+        file += ".ogg"
     snd = pygame.Sound(f"{globals.SND_DIRECTORY}{file}")
     snd.set_volume(globals.volume)
 
