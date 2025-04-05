d8e3f25d075484a95b23cc67471388e4853835e7
Hex <mrbigheadhq@gmail.com>
2025-04-05 18:09:49 +0100

    offset improvements

--- src/states/offseteditor.py
+++ src/states/offseteditor.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, pyautogui, os, shutil
 
 import globals
 from globals import clock, screen, FPS, MUS_DIRECTORY
@@ -20,17 +20,23 @@ char:Character = None
 ghostchar:Character = None
 backsprite:StaticSprite = None
 cross:StaticSprite = None
-scoretext:pygame.Surface = None
+tutorialtext:pygame.Surface = None
 
 curanim = None
 anims = None
 
-charname = "cyan"
+charname = "dude"
 
 cam_x,cam_y = -200,-400
 
 def init():
-    global grp,backsprite,char,cross,curanim,anims,ghostchar,scoretext
+    global grp,backsprite,char,cross,curanim,anims,ghostchar,tutorialtext,charname
+
+    charname = pyautogui.prompt(text="gimme character name",title="")
+    if not os.path.exists(globals.CHR_DIRECTORY+charname+".json"):
+        pyautogui.alert("character doesnt exist! creating it now")
+        shutil.copy2(globals.CHR_DIRECTORY+"default.json",globals.CHR_DIRECTORY+charname+".json")
+
     grp = Camera()
 
     char = Character.load_from_json(charname)
@@ -52,7 +58,7 @@ def init():
     cross = StaticSprite(-13/2,-13/2,load_image("menus/offset/crosshair"))
 
     # i needed the symbols
-    scoretext = globals.default_font.render(f"""OFFSETTER 2000 (tm)
+    tutorialtext = globals.default_font.render(f"""OFFSETTER 2000 (tm)
 A and D switch through animations
 arrow keys move around the camera
 IJKL moves around the character's offset
@@ -75,7 +81,7 @@ def update_ofs():
     return ofsx,ofsy
 
 def run():
-    global cam_x,cam_y,backsprite,char,curanim,anims,ghostchar,scoretext
+    global cam_x,cam_y,backsprite,char,curanim,anims,ghostchar,tutorialtext
     print("we workign")
     space_pressed,left_pressed,right_pressed = False,False,False
     ofs_left_pressed,ofs_right_pressed,ofs_up_pressed,ofs_down_pressed = False,False,False,False
@@ -94,7 +100,7 @@ def run():
         if keys[pygame.K_ESCAPE]:
             globals.gamestate = "freeplay"
             return
-
+        
         if keys[pygame.K_LCTRL] and keys[pygame.K_s] and not save_pressed:
             save_pressed = True
             json_name = globals.CHR_DIRECTORY + charname + ".json"
@@ -142,7 +148,7 @@ def run():
         elif not keys[pygame.K_d]:
             right_pressed = False
 
-        speed = 3
+        speed = 1
         if keys[pygame.K_LSHIFT]:
             speed+=3
         if keys[pygame.K_LCTRL]:
@@ -152,37 +158,35 @@ def run():
 
         if keys[pygame.K_j] and not ofs_left_pressed:
             ofs_left_pressed = True
-            char.animations[anims[curanim]]["offsets"] = [round(ofsx-speed*dt),ofsy]
+            char.animations[anims[curanim]]["offsets"] = [ofsx-speed,ofsy]
             ofsx, ofsy = update_ofs()
         elif not keys[pygame.K_j]:
             ofs_left_pressed = False
 
         if keys[pygame.K_l] and not ofs_right_pressed:
             ofs_right_pressed = True
-            char.animations[anims[curanim]]["offsets"] = [round(ofsx+speed*dt),ofsy]
+            char.animations[anims[curanim]]["offsets"] = [ofsx+speed,ofsy]
             ofsx, ofsy = update_ofs()
         elif not keys[pygame.K_l]:
             ofs_right_pressed = False
 
         if keys[pygame.K_i] and not ofs_up_pressed:
             ofs_up_pressed = True
-            char.animations[anims[curanim]]["offsets"] = [ofsx,round(ofsy-speed*dt)]
+            char.animations[anims[curanim]]["offsets"] = [ofsx,ofsy-speed]
             ofsx, ofsy = update_ofs()
         elif not keys[pygame.K_i]:
             ofs_up_pressed = False
 
         if keys[pygame.K_k] and not ofs_down_pressed:
             ofs_down_pressed = True
-            char.animations[anims[curanim]]["offsets"] = [ofsx,round(ofsy+speed*dt)]
+            char.animations[anims[curanim]]["offsets"] = [ofsx,ofsy+speed]
             ofsx, ofsy = update_ofs()
         elif not keys[pygame.K_k]:
             ofs_down_pressed = False
 
 
-        char.update_rect()
-
         ghostchar.animations["idle"]["offsets"] = char.animations["idle"]["offsets"]
-        ghostchar.update_rect()
+        #ghostchar.update_rect()
 
         backsprite.scroll_factor = [0,0]
         backsprite.update_image()
@@ -191,7 +195,7 @@ def run():
 
         grp.draw(screen)
         grp.update(dt)
-        screen.blit(scoretext,(10,10))
+        screen.blit(tutorialtext,(10,10))
 
         globals.screen_shader.render()
         pygame.display.flip()
