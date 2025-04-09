87ebc021ef02cd49af5990adc30c5e0a8c4fc078
ms-kaylaa <108439098+ms-kaylaa@users.noreply.github.com>
2025-04-08 20:45:01 -0400

    bwords finale (most likely)
    ignore double spacing the font blows hard

--- src/states/bwords.py
+++ src/states/bwords.py
@@ -1,27 +1,44 @@
 import pygame
 
+import random
+
 import globals
 from globals import screen, FPS, small_font, MUS_DIRECTORY, clock
 
 from classes.upscale_group import UpscaleGroup
 
+from util.awesome_util import get_discord_pfp
 from util.conductor import Conductor
 
 texts:list[pygame.Surface] = []
 
+kayla_pfp:pygame.Surface = None
+hex_pfp:pygame.Surface = None
+
 conductor:Conductor = None
 last_beat = -1
 
+introtexts = []
+chosentext = []
+
 def init():
-    global last_beat, conductor, texts
+    global last_beat, conductor, texts, kayla_pfp,hex_pfp, introtexts, chosentext
     last_beat = -1
 
     conductor = Conductor()
     conductor.bpm = 115
 
+    kayla_pfp = pygame.transform.smoothscale_by(get_discord_pfp(1135951334651207701), 0.5)
+    hex_pfp = get_discord_pfp(587298000439869463, "small") # little image for little contributions!
+
     pygame.mixer.music.load(MUS_DIRECTORY + "mus_game.ogg")
     pygame.mixer.music.play(-1)
-    pass
+
+    with open(globals.DATA_DIRECTORY + "introtexts.txt") as f:
+        for line in f.readlines():
+            introtexts.append(line.split("--"))
+
+    chosentext = random.choice(introtexts)
 
 def run():
     global last_beat, conductor,texts
@@ -43,10 +60,13 @@ def run():
         if conductor.beat > last_beat:
             last_beat = conductor.beat
             handle_beatstep()
-            print("beat hit")
 
         for text in texts:
             screen.blit(text[0],text[1])
+
+        if conductor.beat == 7 and (kayla_pfp != None and hex_pfp != None):
+            screen.blit(kayla_pfp, kayla_pfp.get_rect(center=(400,550)))
+            screen.blit(hex_pfp, (10, 800-hex_pfp.height-10))
 
         globals.screen_shader.render()
         pygame.display.flip()
@@ -54,26 +74,26 @@ def run():
 def handle_beatstep():
     match conductor.beat:
         case 0:
-            draw_text("FREE DOWNLOAD BY")
+            draw_text("FREE DOWNLOAD  BY")
         case 2:
-            draw_text("TYLER_MON & FUNNE",1)
+            draw_text("TYLER_MON  &  FUNNE",1)
         case 4:
             texts.clear()
-            draw_text("PORTED TO")
+            draw_text("PORTED  TO", -1)
         case 6:
-            draw_text("PYGAME BY",1)
+            draw_text("PYGAME  BY",)
         case 7:
-            draw_text("KAYLA",2)
+            draw_text("KAYLA",1)
             # i'm not making a function for this
-            newtext = small_font.render("with contributions from hexose :]", False, (255,255,255))
+            newtext = small_font.render("with  contributions  from  hexose  :]", False, (255,255,255))
             newtext = pygame.transform.scale_by(newtext, 2)
-            rect = newtext.get_rect(center=(400,600))
+            rect = newtext.get_rect(bottomleft=(150,790))
             texts.append([newtext,rect])
         case 8:
             texts.clear()
-            draw_text("like this")
+            draw_text(chosentext[0])
         case 10:
-            draw_text("like this",1)
+            draw_text(chosentext[1],1)
         case 12:
             texts.clear()
             draw_text("FNF")
@@ -82,7 +102,7 @@ def handle_beatstep():
         case 14:
             draw_text("DOWNLOAD",2)
         case 15:
-            draw_text("PYGAME PORT",3)
+            draw_text("PYGAME  PORT",3)
         case 16:
             globals.gamestate = "freeplay"
         
--- src/util/awesome_util.py
+++ src/util/awesome_util.py
@@ -1,6 +1,12 @@
 import pygame
 
+import io
+import json
+import requests
+
 import globals
+
+from util import loader
 
 def lerp(a, b, f):
     '''
@@ -27,3 +33,15 @@ def play_snd(file:str):
     snd.set_volume(globals.volume)
 
     return snd.play()
+
+# should this be in loader?
+# maybe...
+
+# this functions cool because the load speed for the video game actively depends on your internet speed!
+def get_discord_pfp(id:int, imagesize = "") -> pygame.Surface:
+    try:
+        r = requests.get(f"http://avatar-cyan.vercel.app/api/pfp/{id}/{imagesize + "image"}")
+        return pygame.image.load(io.BytesIO(r.content))
+    except requests.ConnectionError as e:
+        print("couldnt get pfp for some reason: " + str(e))
+        return loader.load_image(f"backuppfps/bk{imagesize}")
